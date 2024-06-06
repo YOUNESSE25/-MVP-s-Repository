071f428e962438bf915efc036eecf2cf15fd0ce6
MMMBenchahyd <mohamedbenchahyd252@gmail.com>
2024-06-06 20:13:25 +0100

    fix it

--- dl-build.py
+++ dl-build.py
@@ -7,62 +7,56 @@ from img_dn import download_docker_image, extract_image_layers
 
 todo = []
 cmnd = {"FROM": "img", "COPY": "cp", "MKDIR": "mkdir", "CMD": "edit", "CD": "cd"}
-image_id = str(uuid4())
 downld_dest = "/etc/DL/builder/downloaded_images"
 
 
-class run_cmd:
+class RunCmd:
 
     def __init__(self):
-        image_id = str(uuid4())
+        self.image_id = str(uuid4())
         os.makedirs(downld_dest, exist_ok=True)
-        pass
 
     def img(self, line=None):
         lst_line = line.split()[1].split(":")
-        image_name = lst_line[1].strip()
+        image_name = lst_line[0]
         os.makedirs(downld_dest, exist_ok=True)
 
         # Download image
-        # subprocess.run(
-        #     ["curl", "-o", f"{image_name}.tar", f"https://hub.docker.com/{image_name}"]
-        # )
-        # print("Image downloaded")
         download_docker_image(image_name, "latest", downld_dest)
 
         # Extract image
-        # subprocess.run(["tar", "-xzf", f"{image_name}.tar"])
         extract_image_layers(downld_dest, image_name, "latest")
 
         # Fork process
         pid = os.fork()
         if pid == 0:
-            os.chroot(f"{downld_dest}")
-            txt = "nameserver 1.1.1.1"
+            os.chroot(downld_dest)
             with open("/etc/resolv.conf", "w") as resolv_conf:
-                resolv_conf.write(txt)
+                resolv_conf.write("nameserver 1.1.1.1")
             subprocess.run(["apk", "upgrade"])
             subprocess.run(["apk", "update"])
-            subprocess.run(["apk", "add", lst_line[1]])
+            subprocess.run(["apk", "add", image_name])
         else:
             os.wait()
         print("Image is ready")
 
     def cp(self, line=None):
         lst_line = line.split()
-        subprocess.run(["cp"] + lst_line[1:])
+        cmd_2 = os.path.join(downld_dest, lst_line[2])
+        subprocess.run(["cp", lst_line[1], cmd_2])
 
     def mkdir(self, line=None):
         lst_line = line.split()
-        subprocess.run(["mkdir", "-p", lst_line[1]])
+        new_dir = os.path.join(downld_dest, lst_line[1])
+        os.makedirs(new_dir, exist_ok=True)
 
     def cd(self, line=None):
+        global downld_dest
         lst_line = line.split()
-        downld_dest = os.path.join(downld_dest, lst_line)
+        downld_dest = os.path.join(downld_dest, lst_line[1])
 
     def edit(self, line=None):
-        """edit function that build a shell script"""
-
+        """Edit function that builds a shell script"""
         lst_line = line.split()
         cmd = " ".join(lst_line[1:])
 
@@ -71,20 +65,28 @@ class run_cmd:
             f.write(cmd + "\n")
 
         subprocess.run(["chmod", "+x", "startpoint.sh"])
-        subprocess.run(["cp", "startpoint.sh", f"{downld_dest}"])
-        pid = os.fork()
-        if pid == 0:
-            os.chroot(f"{downld_dest}")
-            subprocess.run(["tar", "-cf", f"image_{image_id}.tar", "*"])
+        subprocess.run(["cp", "startpoint.sh", downld_dest])
+
+        os.chdir(downld_dest)
+
+        # Check if there are files to be archived
+        if os.listdir(downld_dest):
+            subprocess.run(["tar", "-cf", f"image_{self.image_id}.tar", "*"])
+            subprocess.run(
+                [
+                    "cp",
+                    os.path.join(downld_dest, f"image_{self.image_id}.tar"),
+                    "/home/azarus/ALX-Project",
+                ]
+            )
         else:
-            os.wait()
-            subprocess.run(["cp", f"{downld_dest}/image_{image_id}.tar", f"."])
+            print("No files to archive in the directory.")
 
 
 with open("DLfile", "r") as f:
     for line in f:
         todo = line.split()
         if todo[0] in cmnd:
-            cls = run_cmd()
+            cls = RunCmd()
             method = getattr(cls, cmnd[todo[0]])
             method(line)
